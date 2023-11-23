be96be35f55e16096778006d96c81c3fc9734ca3
Emiliano Corvalan <emiliano.corvalan@gmail.com>
2023-11-23 15:48:48 -0300

    add comments

--- clientPOO.py
+++ clientPOO.py
@@ -1,19 +1,18 @@
 import socket
 
 def cliente():
-    # Lógica para el programa cliente
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as cliente:
-        cliente.connect(('127.0.0.1', 2345))
+        cliente.connect(('127.0.0.1', 2345)) #conexion con el server en el puerto 2345, en la ip dada.
 
-        fecha = input("Ingrese la fecha en formato YYYY-MM-DD: ")
+        fecha = input("Ingrese la fecha en formato YYYY-MM-DD: ") #solicita fecha para consultar al server
 
-        # Envia la fecha al servidor
+        #envia la fecha al servidor
         cliente.sendall(fecha.encode('utf-8'))
 
-        # Recibe la respuesta del servidor
+        #recibe la respuesta del servidor
         respuesta = cliente.recv(1024).decode('utf-8')
 
         print(respuesta)
 
 if __name__ == "__main__":
-    cliente()
+    cliente() #ejecuta la funcion cuando el script se ejecuta como programa independiente.
